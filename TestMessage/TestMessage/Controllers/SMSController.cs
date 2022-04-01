5c7f1b24b6c8e367a523a2cbd3dfca25d197d4f7
ciaran.russell <ciaran.russell92@gmail.com>
2022-04-01 23:46:32 +0100

    Adding TestMessage Project

---
+++ TestMessage/TestMessage/Controllers/SMSController.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using TestMessage.Models;
+
+namespace TestMessage.Controllers
+{
+    public class SMSController : Controller
+    {
+        
+
+        // GET: SMSController/Details/5
+        public ActionResult Confirm(SMSMessage msg)
+        {
+            return View(msg);
+        }
+
+        // GET: SMSController/Create
+        
+        public ActionResult Send()
+        {
+            return View();
+        }
+
+        // POST: SMSController/Create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Send(SMSMessage msg)
+        {
+            try
+            {
+                return RedirectToAction("Confirm", msg);
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+      
+    }
+}
---
+++ TestMessage/TestMessage/Models/SMSMessage.cs
@@ -0,0 +1,20 @@
+﻿using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+
+namespace TestMessage.Models
+{
+    public class SMSMessage
+    {
+        [Required(ErrorMessage ="Need a Number!")]
+        [StringLength(10, MinimumLength = 10, ErrorMessage = "Phone Number must be 10 digits")]
+        [DisplayName("To")]
+        public string ToMobileNumber { get; set; }
+
+        [Required(ErrorMessage = "Message must not be blank")]
+        [StringLength(140, ErrorMessage = "Max Message 140 Characters")]
+        [DisplayName("Message")]
+        public string Content { get; set; }
+
+
+    }
+}
