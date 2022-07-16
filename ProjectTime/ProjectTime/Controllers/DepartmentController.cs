145cf7a8272a6ea594ad8f941261f4e8088e0e19
ciaran.russell <ciaran.russell92@gmail.com>
2022-07-16 20:51:03 +0100

    First Commit

---
+++ ProjectTime/ProjectTime/Controllers/DepartmentController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using ProjectTime.Data;
+using ProjectTime.Models;
+
+namespace ProjectTime.Controllers
+{
+    public class DepartmentController : Controller
+    {
+        private readonly ApplicationDbContext _db;
+
+        public DepartmentController(ApplicationDbContext db)
+        {
+            _db = db;
+        }
+
+        public IActionResult Index()
+        {
+            IEnumerable<Department> objDepartmentList = _db.departments;
+            return View(objDepartmentList);
+        }
+    }
+}
---
+++ ProjectTime/ProjectTime/Models/Department.cs
@@ -0,0 +1,20 @@
+﻿using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+
+namespace ProjectTime.Models
+{
+    public class Department
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required(ErrorMessage = "Department name is required")]
+        [DisplayName("Department Name")]
+        public string Name { get; set; }
+        [Required]
+        [DisplayName("Rate/Hr")]
+        public decimal Rate { get; set; }
+        public DateTime CreateDateTime { get; set; } = DateTime.Now;
+
+    }
+}
